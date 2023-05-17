fdae1f3221d054b61034b8786cfce7e15f4bc337
JODAHAN <dan0748@naver.com>
2023-05-18 06:16:31 +0900

    수정

--- server/server.js
+++ server/server.js
@@ -12,17 +12,13 @@ const app = express();
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-app.use(
-  cors({ credentials: true, origin: "https://hwayang-admin.vercel.app/" })
-);
-app.use(
-  cors({ credentials: true, origin: "https://hwayang-admin.vercel.app/" })
-);
+app.use(cors());
 
 app.use("/uploads", express.static(path.join(__dirname, "./uploads")));
 app.use("/api/admin", adminRouter);
 
-app.listen(process.env.PORT, () => {
-  console.log(`Example app listening on port ${process.env.PORT}`);
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`);
 });
